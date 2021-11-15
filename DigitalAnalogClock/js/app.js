08404a48b80749b375b16fb5f576c47da1c977c0
Noual Ali <noualdev@gmail.com>
2021-11-15 19:05:06 +0100

    Analog clock

--- DigitalAnalogClock/js/app.js
+++ DigitalAnalogClock/js/app.js
@@ -1,36 +1,73 @@
+function currentTime() {
+    return {
+        now: new Date(),
+        get hour() { return this.now.getHours() },
+        get minute() { return this.now.getMinutes() },
+        get second() { return this.now.getSeconds() },
+    }
+}
+
 /**
  * Update digital clock
  * 
  * @return {void}
  */
-function updateDigitalClock() {
+function digitalClock() {
+
     const digitalClock = document.querySelector('#digital-clock')
     const hoursElt = digitalClock.querySelector('#hours')
     const minutesElt = digitalClock.querySelector('#minutes')
     const secondsElt = digitalClock.querySelector('#seconds')
 
-    const today = new Date()
-    const currentHour = today.getHours()
-    const currentMinute = today.getMinutes()
-    const currentSecond = today.getSeconds()
-
-    if (hoursElt.textContent != currentHour) {
-        hoursElt.textContent = addZero(currentHour)
+    /**
+     * For performance optimization update only if the value changes
+     */
+    if (hoursElt.textContent != currentTime().hour) {
+        hoursElt.textContent = addZero(currentTime().hour)
     }
-
-    if (minutesElt.textContent != currentMinute) {
-        minutesElt.textContent = addZero(currentMinute)
+    
+    if (minutesElt.textContent != currentTime().minute) {
+        minutesElt.textContent = addZero(currentTime().minute)
     }
-
-    if (secondsElt.textContent != currentSecond) {
-        secondsElt.textContent = addZero(currentSecond)
+    
+    if (secondsElt.textContent != currentTime().second) {
+        secondsElt.textContent = addZero(currentTime().second)
     }
 }
 
-updateDigitalClock()
+/**
+ * Analog clock
+ * 
+ * @return {void}
+ */
+function analogClock() {
+    const analogClock = document.querySelector('#analog-clock')
+    const hoursElt = analogClock.querySelector('.hand.hour')
+    const minutesElt = analogClock.querySelector('.hand.minute')
+    const secondsElt = analogClock.querySelector('.hand.second')
+    
+    
+    const secondsRatio = currentTime().second / 60
+    const minutesRatio = (secondsRatio + currentTime().minute) / 60
+    const hoursRatio = (minutesRatio + currentTime().hour) / 12
+    
+    setRotation(secondsElt, secondsRatio)
+    setRotation(minutesElt, minutesRatio)
+    setRotation(hoursElt, hoursRatio)
+    
+}
 
-setInterval(() => updateDigitalClock(), 1000)
 
+/**
+ * Update hands rotation
+ * @param {HTMLDivElement} elt
+ * @param {String|Number} rotationRatio
+ * 
+ * @return {void}
+ */
+function setRotation(elt, rotationRatio) {
+    elt.style.setProperty('--rotate', rotationRatio * 360)
+}
 
 /**
  * Add zero for numbers smaller than 10
@@ -42,3 +79,12 @@ function addZero(str) {
     str = Number.parseInt(str)
     return str < 10 ? '0' + str : str
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    digitalClock()
+    analogClock()
+    setInterval(() => {
+        digitalClock()
+        analogClock()
+    }, 1000)
+})
